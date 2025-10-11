23ee01559936411972fdc9d541e1081e59453ee5
MMurdock17 <mattiemurdock1@gmail.com>
2025-10-11 06:36:51 -0400

    Getting assets in, starting player movement

---
+++ Project B/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+public float speed = 5;
+public Rigidbody2D rb;
+
+
+    // Update is called 50x per frame
+    void FixedUpdate()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        rb.linearVelocity = new Vector2(horizontal, vertical) * speed;
+    }
+}
