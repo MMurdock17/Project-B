5f76bd3b55adfc08b4002e66003839ef1e2bbca0
MMurdock17 <mattiemurdock1@gmail.com>
2025-10-11 07:08:59 -0400

    Getting movement animations in place

--- Project B/Assets/Scripts/PlayerMovement.cs
+++ Project B/Assets/Scripts/PlayerMovement.cs
@@ -5,6 +5,8 @@ public class PlayerMovement : MonoBehaviour
 
 public float speed = 5;
 public Rigidbody2D rb;
+public Animator anim;
+public int facingDirection = 1;
 
 
     // Update is called 50x per frame
@@ -13,6 +15,21 @@ public Rigidbody2D rb;
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
 
+        if (horizontal > 0 && transform.localScale.x < 0 || horizontal < 0 && transform.localScale.x > 0)
+        {
+            Flip();
+        }
+
+        anim.SetFloat("horizontal", Mathf.Abs(horizontal));
+        anim.SetFloat("vertical", Mathf.Abs(vertical));
+
         rb.linearVelocity = new Vector2(horizontal, vertical) * speed;
     }
+
+    void Flip()
+    {
+        facingDirection *= -1;
+        transform.localScale = new Vector3(transform.localScale.x * -1, transform.localScale.y, transform.localScale.z);
+    }
+
 }
