20b7d738c2994fa3dfb94cab2ae21d8f0b9b853b
MMurdock17 <mattiemurdock1@gmail.com>
2025-10-15 01:31:16 -0400

    Setting up enemy flipping

---
+++ Project B/Assets/Scripts/Enemy/EnemyMovement.cs
@@ -0,0 +1,62 @@
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+
+    public float speed;
+    private bool isChasing;
+    private Rigidbody2D rb;
+    private Transform player;
+    private int facingDirection = 1;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+        rb = GetComponent<Rigidbody2D>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isChasing == true)
+        {
+
+            if (player.position.x > transform.position.x && facingDirection == 1 || player.position.x < transform.position.x && facingDirection == -1);
+            {
+                Flip();
+            }
+            
+            Vector2 direction = (player.position - transform.position).normalized;
+            rb.linearVelocity = direction * speed;
+        }
+    }
+
+    void Flip()
+    {
+        facingDirection *= -1;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            if (player == null)
+            {
+                player = collision.transform;
+            }
+            isChasing = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            rb.linearVelocity = Vector2.zero;
+            isChasing = false;
+        }
+    }
+
+}
