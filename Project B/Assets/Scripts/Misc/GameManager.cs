b6c1aaeb2d92536ad5fdc5378c9c3dae644bd70e
MMurdock17 <mattiemurdock1@gmail.com>
2025-10-20 01:41:58 -0400

    Update GameManager.cs

--- Project B/Assets/Scripts/Misc/GameManager.cs
+++ Project B/Assets/Scripts/Misc/GameManager.cs
@@ -25,4 +25,8 @@ public class GameManager : MonoBehaviour
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
+    public void MainMenu()
+    {
+        SceneManager.LoadScene("Menu");
+    }
 }
